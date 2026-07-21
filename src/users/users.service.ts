281c4c7ac1a80465b0b4f6aaaf138319f1ac7e5c
Le Gia Huy <legiahuy12072050@gmail.com>
2026-07-21 14:16:44 +0700

    refactor: free plan reconcilliation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,8 +4,13 @@ import {
   Logger,
 } from "@nestjs/common";
 import { PrismaService } from "../database/prisma.service";
-import { User } from "@prisma/client";
+import { User, SubscriptionStatus } from "@prisma/client";
 import { randomUUID } from "crypto";
+
+const LIVE_STATUSES: SubscriptionStatus[] = [
+  SubscriptionStatus.ACTIVE,
+  SubscriptionStatus.PAST_DUE,
+];
 
 @Injectable()
 export class UsersService {
@@ -37,7 +42,10 @@ export class UsersService {
     return this.prisma.user.findMany({
       where: {
         createdAt: { lt: params.createdBefore },
-        OR: [{ providerCustomerId: null }, { subscriptions: { none: {} } }],
+        OR: [
+          { providerCustomerId: null },
+          { subscriptions: { none: { status: { in: LIVE_STATUSES } } } },
+        ],
       },
       orderBy: { createdAt: "asc" },
       take: params.limit,
